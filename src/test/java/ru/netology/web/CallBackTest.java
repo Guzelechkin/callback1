ddf461b4cd95c24543a1b6c1faf3ae905d470561
Guzelechkin <cot-oc-pb@mail.ru>
2024-02-23 12:01:18 +0500

    initial2

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -19,10 +19,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class CallbackTest {
     private WebDriver driver;
 
-    @BeforeAll
-    static void setUpAll() {
-        WebDriverManager.chromedriver().setup();
-    }
 
     @BeforeEach
     void setUp() {
